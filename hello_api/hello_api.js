119b700679670c88978db81d64bddb6225bd0e74
Darren Kessner <Darren.Kessner@marlborough.org>
2022-02-23 16:33:40 -0800

    modified:   hello_api.js

--- hello_api/hello_api.js
+++ hello_api/hello_api.js
@@ -5,28 +5,52 @@ function setup() {
 
 let x = 50;
 
+
 let catimg;
+let catdata;
+
 
 function draw() 
 {
     background(0);
+
     if (catimg)
-        image(catimg, 0, 0);
+    {
+        let w = 0, h = 0;
+
+        if (catdata.width > catdata.height)
+            w = width;
+        else
+            h = height;
+
+        image(catimg, 0, 0, w, h);
+    }
+    else if (catimg === null)
+    {
+        fill(255);
+        text("Loading", width/2, height/2);
+    }
 }
 
 
 function mousePressed()
 {
-    let caturl = "https://api.thecatapi.com/v1/images/search";
+    catimg = null;
+    const caturl = "https://api.thecatapi.com/v1/images/search";
     loadJSON(caturl, getRandomCatPic);
 }
 
 
 function getRandomCatPic(data)
 {
-    console.log(data);
+    catdata = data[0];
+    console.log(catdata);
 
-    catimg = createImg(data[0].url, "noimage");
-    //catimg.resize(width, height); // throws errors 
+    catimg = createImg(catdata.url, "random cat image", null, () => {
+        textAlign(CENTER);
+        console.log("Loaded: " + catdata.url);
+        catimg.hide();
+    });
+
 }
 
